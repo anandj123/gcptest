352e115485c8aecfac553ee1b73d3b30b3eeaaa4
Anand Jain <jain.12@gmail.com>
2021-02-05 12:18:39 -0500

    test

--- techbash-aiml/v2/f05.sh
+++ techbash-aiml/v2/f05.sh
@@ -1,14 +1,14 @@
 touch $SRC_DIR/triggers/f05.s
 while :
 do
-    if [ -f "$SRC_DIR/triggers/f01.f" ] 
+    if [ -f "$SRC_DIR/triggers/f01.f" ] && [ -f "$SRC_DIR/triggers/f03.f" ]
     then
 
 # Todo: check if the service is accepting traffic
-        export SERVICE_URL=$(gcloud run services list --platform managed |tail -1 |awk '{print $4}')
-        curl -vX POST $SERVICE_URL/json -d @../ecommerce_view_event.json --header "Content-Type: application/json"
-        curl -vX POST $SERVICE_URL/json -d @../ecommerce_add_to_cart_event.json --header "Content-Type: application/json"
-        curl -vX POST $SERVICE_URL/json -d @../ecommerce_purchase_event.json --header "Content-Type: application/json"
+export SERVICE_URL=$(gcloud run services list --platform managed |tail -1 |awk '{print $4}')
+curl -vX POST $SERVICE_URL/json -d @../ecommerce_view_event.json --header "Content-Type: application/json"
+curl -vX POST $SERVICE_URL/json -d @../ecommerce_add_to_cart_event.json --header "Content-Type: application/json"
+curl -vX POST $SERVICE_URL/json -d @../ecommerce_purchase_event.json --header "Content-Type: application/json"
 #Todo: Check if the output of the post is successful
 
         break
