9edc7b6a621f40bacad29b770a07dd54603e3d8c
Anand Jain <jain.12@gmail.com>
2021-02-05 10:45:52 -0500

    test

--- techbash-aiml/v2/f04.sh
+++ techbash-aiml/v2/f04.sh
@@ -6,12 +6,11 @@ do
     if [ -f "$SRC_DIR/triggers/f01.f" ] 
     then
         
-        gcloud dataflow jobs run ecommerce-events-ps-to-bq-stream \
-            --gcs-location gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery \
-            --region us-central1 \
-            --staging-location gs://$BUCKET_ID/temp \
-            --parameters \ 
-inputSubscription=projects/$PROJECT/subscriptions/ecommerce-events-pull,\
+gcloud dataflow jobs run ecommerce-events-ps-to-bq-stream \
+--gcs-location gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery \
+--region us-central1 \
+--staging-location gs://$BUCKET_ID/temp \
+--parameters inputSubscription=projects/$PROJECT/subscriptions/ecommerce-events-pull,\
 outputTableSpec=$PROJECT:retail_dataset.ecommerce_events,\
 outputDeadletterTable=$PROJECT:retail_dataset.ecommerce_events_dead
 
