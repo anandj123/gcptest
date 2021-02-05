26ee8ff629c25ff17c45c71c00b46d823aa204ee
Anand Jain <jain.12@gmail.com>
2021-02-05 10:43:40 -0500

    test

--- techbash-aiml/v2/test2.sh
+++ techbash-aiml/v2/test2.sh
@@ -1,8 +1,8 @@
-        gcloud dataflow jobs run ecommerce-events-ps-to-bq-stream \
-            --gcs-location gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery \
-            --region us-central1 \
-            --staging-location gs://$BUCKET_ID/temp \
-            --parameters \ 
+gcloud dataflow jobs run ecommerce-events-ps-to-bq-stream \
+--gcs-location gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery \
+--region us-central1 \
+--staging-location gs://$BUCKET_ID/temp \
+--parameters \ 
 inputSubscription=projects/$PROJECT/subscriptions/ecommerce-events-pull,\
 outputTableSpec=$PROJECT:retail_dataset.ecommerce_events,\
 outputDeadletterTable=$PROJECT:retail_dataset.ecommerce_events_dead
