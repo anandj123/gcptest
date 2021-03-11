68c179f040156093576be90cba67bdd0043e52d9
Anand Jain <jain.12@gmail.com>
2021-03-11 08:17:16 -0500

    data flow labels test

--- SpannerInterleave/main.py
+++ SpannerInterleave/main.py
@@ -2,6 +2,7 @@ import sys
 from google.cloud import spanner
 import time
 import random
+import string
 
 def query_a(instance_id, database_id, query):
     spanner_client = spanner.Client()
@@ -15,13 +16,13 @@ def query_a(instance_id, database_id, query):
         )
 
     t_start = time.time()
-    print("Query A: Iteration Time (seconds)")
-    print("------------------------------------")
+    print("Query (Non-primary key): Iteration Time (seconds)")
+    print("--------------------------------------------------")
     for i in range(100):
         i_start = time.time()
         
         with database.snapshot() as snapshot:
-            r = random.randrange(2000000)
+            r = random.randrange(1000000)
             results = snapshot.execute_sql(
                 query,
                 params={"lastName": "last " + str(r)},
@@ -30,13 +31,10 @@ def query_a(instance_id, database_id, query):
 
         #for row in results:
         #    print(u"SingerId: {}, FirstName: {}, LastName: {}".format(*row))
-        print("%s" % (time.time() - i_start))
+        #print("%s" % (time.time() - i_start))
     
-    print("------------------------------------")
-    print("Total Time (seconds)")
-    print("------------------------------------")
+    print("Total Time (seconds) : %s \n" % (time.time() - t_start))
     
-    print("%s" % (time.time() - t_start))
 
 def query_b(instance_id, database_id, query):
     spanner_client = spanner.Client()
@@ -50,13 +48,13 @@ def query_b(instance_id, database_id, query):
         )
 
     t_start = time.time()
-    print("Query B: Iteration Time (seconds)")
-    print("------------------------------------")
+    print("Query (primary key): Iteration Time (seconds)")
+    print("----------------------------------------------")
     for i in range(100):
         i_start = time.time()
         
         with database.snapshot() as snapshot:
-            r = random.randrange(2000000)
+            r = random.randrange(1000000)
             results = snapshot.execute_sql(
                 query,
                 params={"SingerId": r},
@@ -65,16 +63,12 @@ def query_b(instance_id, database_id, query):
 
         #for row in results:
         #    print(u"SingerId: {}, FirstName: {}, LastName: {}".format(*row))
-        print("%s" % (time.time() - i_start))
+        #print("%s" % (time.time() - i_start))
     
-    print("------------------------------------")
-    print("Total Time (seconds)")
-    print("------------------------------------")
-    
-    print("%s" % (time.time() - t_start))
+    print("Total Time (seconds) : %s \n" % (time.time() - t_start))
 
 
-def insert_data(instance_id, database_id):
+def insert_data(instance_id, database_id,num_batches):
     """Inserts sample data into the given database.
 
     The database and table must already exist and can be created using
@@ -90,34 +84,42 @@ def insert_data(instance_id, database_id):
         batch.delete("Albums", remaining_singers)
         print('Truncatd Singers and Album tables.')
             
-    for j in range(1000):
-        batchsize = 2000
+    for j in range(int(num_batches)):
+        batchsize = 1000
         with database.batch() as batch:
             data = []
             for i in range(batchsize):
                 k = j*batchsize + i
                 data.append((k,u'first ' + str(k) , u'last '+ str(k) ))
             #print(data)
-            
-            batch.insert(
-                table="Singers",
-                columns=("SingerId", "FirstName", "LastName"),
-                values=data,
-            )
+            try:            
+                batch.insert(
+                    table="Singers",
+                    columns=("SingerId", "FirstName", "LastName"),
+                    values=data,
+                )
+            except:
+                print("Exception occured")
+
             data = []
             for i in range(batchsize):
                 k = j*batchsize + i
-                data.append((k,k, u'Title ' + str(k) ))
+                title = ''.join(random.choice(string.ascii_lowercase) for i in range(4000))
+                data.append((k,k, title))
+            try:
+                batch.insert(
+                    table="Albums",
+                    columns=("SingerId", "AlbumId", "AlbumTitle"),
+                    values=data,
+                )
+            except:
+                print("Exception occured")
 
-            batch.insert(
-                table="Albums",
-                columns=("SingerId", "AlbumId", "AlbumTitle"),
-                values=data,
-            )
+    
             #batch.commit()
         print("Inserted batch : " + str(j))
 
-def insert_data2(instance_id, database_id):
+def insert_data2(instance_id, database_id,num_batches):
     """Inserts sample data into the given database.
 
     The database and table must already exist and can be created using
@@ -133,39 +135,53 @@ def insert_data2(instance_id, database_id):
         batch.delete("Albums2", remaining_singers)
         print('Truncatd Singers2 and Album2 tables.')
             
-    for j in range(1000):
-        batchsize = 2000
+    for j in range(int(num_batches)):
+        batchsize = 1000
         with database.batch() as batch:
             data = []
             for i in range(batchsize):
                 k = j*batchsize + i
                 data.append((k,u'first ' + str(k) , u'last '+ str(k) ))
             #print(data)
-            
-            batch.insert(
-                table="Singers2",
-                columns=("SingerId", "FirstName", "LastName"),
-                values=data,
-            )
+            try:
+                batch.insert(
+                    table="Singers2",
+                    columns=("SingerId", "FirstName", "LastName"),
+                    values=data,
+                )
+            except:
+                print("Exception occured")
+
+
             data = []
             for i in range(batchsize):
                 k = j*batchsize + i
-                data.append((k,k, u'Title ' + str(k) ))
+                title = ''.join(random.choice(string.ascii_lowercase) for i in range(4000))
+                data.append((k,k, title))
+            try:
+                batch.insert(
+                    table="Albums2",
+                    columns=("SingerId", "AlbumId", "AlbumTitle"),
+                    values=data,
+                )
+            except:
+                print("Exception occured")
 
-            batch.insert(
-                table="Albums2",
-                columns=("SingerId", "AlbumId", "AlbumTitle"),
-                values=data,
-            )
             #batch.commit()
         print("Inserted batch : " + str(j))
 
-    
-
 if __name__ == '__main__':
+    # To call the program 
+    # gcloud auth application-default login
+    # python main.py test-1 test
     #insert_data(*sys.argv[1:])
     #insert_data2(*sys.argv[1:])
-    query_a(*sys.argv[1:],"SELECT SingerId, FirstName, LastName FROM Singers inner join Album using(SingerId) WHERE LastName = @lastName")
-    query_a(*sys.argv[1:],"SELECT SingerId, FirstName, LastName FROM Singers2 inner join Album2 using(SingerId) WHERE LastName = @lastName")
-    query_b(*sys.argv[1:],"SELECT SingerId, FirstName, LastName FROM Singers inner join Album using(SingerId) WHERE SingerId = @SingerId")
-    query_b(*sys.argv[1:],"SELECT SingerId, FirstName, LastName FROM Singers2 inner join Album2 using(SingerId) WHERE SingerId = @SingerId")
+
+    query_a(*sys.argv[1:],"SELECT SingerId FROM Singers inner join Album using(SingerId) WHERE LastName = @lastName")
+    query_a(*sys.argv[1:],"SELECT SingerId FROM Singers2 inner join Album2 using(SingerId) WHERE LastName = @lastName")
+    query_b(*sys.argv[1:],"SELECT SingerId FROM Singers inner join Album using(SingerId) WHERE SingerId = @SingerId")
+    query_b(*sys.argv[1:],"SELECT SingerId FROM Singers2 inner join Album2 using(SingerId) WHERE SingerId = @SingerId")
+
+    query_b(*sys.argv[1:],"SELECT s.FirstName, s.LastName, array(SELECT a1.AlbumTitle FROM Singers s1 JOIN Albums a1 ON s1.SingerId = a1.SingerId WHERE s.SingerId = s1.SingerId) FROM Singers s WHERE EXISTS(SELECT a2.AlbumTitle FROM Albums a2 WHERE a2.SingerId = s.SingerId) AND s.SingerId = @SingerId")
+    query_b(*sys.argv[1:],"SELECT s.FirstName, s.LastName, array(SELECT a1.AlbumTitle FROM Singers2 s1 JOIN Albums2 a1 ON s1.SingerId = a1.SingerId WHERE s.SingerId = s1.SingerId) FROM Singers2 s WHERE EXISTS(SELECT a2.AlbumTitle FROM Albums2 a2 WHERE a2.SingerId = s.SingerId) AND s.SingerId = @SingerId")
+
