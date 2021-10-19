85c814daff02ec8ecc112e9f1ec6a03364e49c07
Anand Jain <jain.12@gmail.com>
2021-10-19 19:47:07 -0400

    Initial script

--- sql_column_renames/1.sql
+++ sql_column_renames/1.sql
@@ -1,18 +1,24 @@
 create volatile table ZZRF04, no fallback, no log(
-	SKU_KEY	INTEGER, 
+	EC_ORDER_ITLSHP_ID	INTEGER, 
 	GODWFLAG6_1	INTEGER, 
 	GODWFLAG8_1	INTEGER)
-primary index (SKU_KEY) on commit preserve rows
+primary index (EC_PROFILE_ADDR_TYPE_ID) on commit preserve rows
 
 ;insert into ZZRF04 
 select	/* From MSTR Environment: Dev; Project : DW ; Report= 376B6E7341A57BAB92272DBA8B6477C8 ; User=BC74445 */ distinct r11.SKU_KEY  SKU_KEY,
-	(Case when r11.CHAIN_STATUS in ('A', 'I') then 1 else 0 end)  GODWFLAG6_1,
-	(Case when r11.CHAIN_STATUS in ('D', 'N') then 1 else 0 end)  GODWFLAG8_1
+	(Case when r11.CREATE_PROCESS_CD in ('A', 'I') then 1 else 0 end)  GODWFLAG6_1,
+	(Case when r11.PROFILE_EMAIL_ADDRESS_ID in ('D', 'N') then 1 else 0 end)  GODWFLAG8_1,
+	mailing_address_id,
+	RTV_TYPE_DESC,
+	CREATE_JOB_INSTANCE_ID,
+	FIPS_STATE_CD,
+	EC_CARRIER_SHP_TRK_ID
+	
 from	DWPSKULU	r11
-where	(r11.CHAIN_STATUS in ('A', 'I')
+where	(r11.EC_ORDER_ITLSHP_ID in ('A', 'I')
  or r11.CHAIN_STATUS in ('D', 'N'))
 
 create volatile table ZZMD05, no fallback, no log(
-	SKU_KEY	INTEGER, 
+	PROD_DSCNT_AMT	INTEGER, 
 	WJXBFS1	INTEGER)
 primary index (SKU_KEY) on commit preserve rows
--- sql_column_renames/mapping.py
+++ sql_column_renames/mapping.py
@@ -1,76 +1,117 @@
+import tokenize
 import codecs
 import json
-class Mapping:
-    def __init__(self, prev_name, new_name, duplicate_name_found, tables):
-        self.prev_name = prev_name
-        self.new_name = new_name
-        self.duplicate_name_found = duplicate_name_found
-        self.tables = tables
+import csv
 
-# import pandas as pd
-# df = pd.read_csv('rearchitect-sheet.csv',encoding='ISO-8859-1', low_memory=False)
-# df = df[df['column_name'].notna()]
-# df = df[df['first_legacy_col_map'].notna()]
-# final_columns = ['column_name', 'first_legacy_col_map']
-# df.drop(columns=[col for col in df if col not in final_columns],inplace=True)
-# df[['system', 'schema','table', 'column']]=df['first_legacy_col_map'].str.split('.',expand=True).iloc[:,[0,1,2,3]]
-# print(df[df['column_name'].duplicated() == True].iloc[:,[0,5]])
+col_mapping_dictionary = {}
+errors = [""]
+errorcount = 0
+skip_header = True
+with open('1.error', 'w') as o:
+    # Input file has some codec issues so ignoring those records with potential issues
+    with codecs.open('rearchitect-sheet.csv', 'r', encoding='utf-8', errors='ignore') as infile:
+        csv_reader = csv.reader(infile)
 
-# print(df)
+        for cols in csv_reader:
+            if (skip_header):
+                skip_header = False
+                continue
 
-# df=df.groupby(['column'], as_index = False).agg({'column_name': ','.join})
-# df[['column','column_name']].drop_duplicates()
-# #df2 = df[['column', 'column_name']].groupby(['column'])['column_name'].transform(lambda x: ','.join(x))
-# print(df)
+            if (cols[0] == ''): # Blank records in csv is skipped
+                continue
+            # if ('analytical' in cols[0] or 'operational' in cols[0].lower()): # Analytical mappings are skipped (new tables in Bigquery)
+            #     continue
 
-# #df2[['column','text','column_name']].drop_duplicates()
-# df.to_csv('out.out',index=False)
+            # If we don't have enough columns in input file it is invalid record skip
+            if (len(cols)<=5): 
+                o.write('Invalid record format: [' + ','.join(cols) + ']\n')
+                errors.append( )
+                errorcount += 1
+                continue
 
-"""
-{
-    prev_name: "address_id",
-    new_name: "addressess_guid",
-    duplicate_name_found: False
-    tables: [
-        {
-            table_name: "NETEZZA.ANALYTICS_TB.ADDRESS_HOME_STORE_MASTER_ST_PROD[XXXX]"
-            new_col_name: "addressess_guid"
-        },
-        {
-            table_name: "NETEZZA.ANALYTICS_TB.ADDRESS_HOME_STORE_MASTER_CF[XXXX]"
-            new_col_name: "addressess_guid"
-        },
-    ]
-}
-"""
-tables = ["NETEZZA.ANALYTICS_TB.ADDRESS_HOME_STORE_MASTER_ST_PROD[XXXX]",
-        "NETEZZA.ANALYTICS_TB.ADDRESS_HOME_STORE_MASTER_CF[XXXX]"]
-m = Mapping("address_id", "address_guid", False, tables)
-j = json.loads(m)
-print(json.dumps(m.__dict__))
-print(j['prev_name'])
-json
-exit()
+            new_name = cols[1] # mapped new column name in Bigquerry
 
-with codecs.open('rearchitect-sheet.csv', 'r', encoding='utf-8', errors='ignore') as infile:
-    counter = 0
-    lines = infile.readlines()
-    for line in lines:
-        cols = line.split(',')
-        if (len(cols)<=5):
-            break
-        new_name = cols[1]
-        prev_full_name = cols[3]
-        prev_col_name = prev_full_name.split('.')
-        if (len(prev_col_name)<4):
+            # previous column name in format: 
+            #   TERADATA.POS_MCF_TB.EC_CARRIER_SHP_TRK_DLVRY_DTL.TRK_SCAN_CITY_CD
+            prev_full_name = cols[3] 
+
+            prev_col_name = prev_full_name.split('.')
+            if (len(prev_col_name)<4): # Invalid format skip it
+                o.writelines('Invalid column format: [' + ','.join(cols) + ']\n')
+                errorcount += 1
+                continue
+
+            # Extract the DW column name as the last value in the split array
+            prev_col_name = prev_col_name[len(prev_col_name) - 1]
+
+            # If both are same then skip it
+            if(new_name == prev_col_name):
+                continue
+            
+            # If we have a column with same name in mapping dictionary we have a duplicate mapping
+            if (prev_col_name in col_mapping_dictionary ):
+
+                (dictionary_name, 
+                dictionary_full_name, 
+                dictionary_duplicate_count) = col_mapping_dictionary[prev_col_name] 
+                
+                if (dictionary_name != new_name):
+                    col_mapping_dictionary[prev_col_name] = (
+                            new_name, 
+                            dictionary_full_name + '\n' + 
+                            '{:80s} -> {:30s}'.format(prev_full_name, new_name), 
+                            dictionary_duplicate_count+1)
+            else:
+                col_mapping_dictionary[prev_col_name] = (new_name, '{:80s} -> {:30s}'.format(prev_full_name, new_name),1)
+    infile.close()
+
+print('Number of errors: ', errorcount)
+
+
+        
+
+# Create output token buffer for printing the output file
+tokensout = []
+
+# Open the input sql file and tokenize it to read each token
+with tokenize.open('1.sql') as f:
+    tokens = tokenize.generate_tokens(f.readline)
+    for token in tokens:
+        # If token type is not NAME then we can skip processing it and just add it to our output list
+        if (token.type != tokenize.NAME):
+            tokensout.append(token)
             continue
-        prev_col_name = prev_col_name[len(prev_col_name) - 1]
-        if(new_name == prev_col_name):
-            continue
-        print(prev_full_name, ': ', new_name, '->',prev_col_name )
-        counter += 1
-        # if counter > 10:
-        #     break;
 
-infile.close()
+        if (token.string in col_mapping_dictionary):
+            (new_name, prev_name, duplicate_count) = col_mapping_dictionary[token.string] 
+
+            # If we found duplicate column name mapping add WARNING comment before the mapping column
+            if (duplicate_count > 1):
+                t = tokenize.TokenInfo(tokenize.NAME,  
+                            '\n\n/*******************************************************************************************************************\n' +
+                            'WARNING DUPLICATE COLUMN NAME FOUND: (' + 
+                            token.string + ')\n\n' + 
+                            prev_name  + 
+                            '\n*********************************************************************************************************************/\n', 
+                            token.start, 
+                            token.end, 
+                            token.line)
+
+                tokensout.append(t)
+                (sline, scol) = token.end
+                t = tokenize.TokenInfo(tokenize.NAME,  new_name , (sline, scol+1) , token.end , token.line)
+                tokensout.append(t)
+            else:
+                t = tokenize.TokenInfo(tokenize.NAME,  new_name , token.start , token.end , token.line)
+                tokensout.append(t)
+
+            #print('Token replaced for : ', token.string)
+        else:
+            tokensout.append(token) # If no mapping found then just keep the same column name
+
+with open('1.out', 'w') as o:
+    o.write(tokenize.untokenize(tokensout))
+
+            
+
 
